5680c698793a941e12a2634bc44886b46ee82c3f
Felipe Manfrin <felipemanfringonsales@gmail.com>
2026-09-14 22:08:58 -0300

    Hello, World!

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,15 +4,22 @@ import { IonicModule } from '@ionic/angular/lazy';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
-import { HomePageRoutingModule } from './home-routing.module';
 
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePage,
+  }
+];
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    RouterModule.forChild(routes)
   ],
   declarations: [HomePage]
 })
