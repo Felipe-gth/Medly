d4f7d4cba31a6b632bf6972477ce2b20306e8628
Felipe Manfrin <felipemanfringonsales@gmail.com>
2026-09-17 22:04:13 -0300

    Create shared folder and module, medicine component and import them

--- src/app/medicines/medicines.module.ts
+++ src/app/medicines/medicines.module.ts
@@ -6,6 +6,7 @@ import { IonicModule } from '@ionic/angular/lazy';
 
 import { Routes, RouterModule } from '@angular/router';
 import { MedicinesPage } from './medicines.page';
+import { SharedModule } from '../shared/shared-module';
 
 
 
@@ -21,7 +22,8 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    SharedModule
   ],
   declarations: [MedicinesPage]
 })
