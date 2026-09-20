e70699a0bc4ee490a6b90a4daffd57112fa4fda1
Felipe Manfrin <felipemanfringonsales@gmail.com>
2026-09-19 23:57:35 -0300

    Change add medicine button and fix empty state/content state exhibition

--- src/app/medicines/medicines.page.ts
+++ src/app/medicines/medicines.page.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MedicinesPage implements OnInit {
 
-  medicines: string[] = [];
+  medicines: string[] = [''];
 
   constructor() { }
 
