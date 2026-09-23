fadb1579182fb5619cc6454d48447ef060082097
Felipe Manfrin <felipemanfringonsales@gmail.com>
2026-09-23 19:59:04 -0300

    Create, declare, import and export EmailValidate directive

--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { MedicineComponent } from './components/medicine/medicine.component';
 import { BottomNavbarComponent } from './components/mobile/bottom-navbar/bottom-navbar.component';
 import { RouterModule } from '@angular/router';
+import { EmailValidatorDirective } from './directives/email-validate.directive';
 
 @NgModule({
-  declarations: [MedicineComponent, BottomNavbarComponent],
+  declarations: [MedicineComponent, BottomNavbarComponent, EmailValidatorDirective],
   imports: [CommonModule, RouterModule],
-  exports: [MedicineComponent, BottomNavbarComponent]
+  exports: [MedicineComponent, BottomNavbarComponent, EmailValidatorDirective]
 })
 export class SharedModule { }
